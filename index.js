507b1dd7ac0337404fd218f0bf79d320e789c03e
WilliamDenega <williamdenega@gmail.com>
2022-02-28 13:03:32 -0500

    commit tree c3926654fabbf3c4863c8c1e966925de0a0e956d

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
   //add static methods here
+
+
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase()+ string.slice(1)
+  }
+  static sanitize(string){
+    //this keeps the spaces in the string
+    return string.replace(/[^A-Za-z0-9 '-]+/g,"")
+  }
+  static titleize(string){
+    let exceptions = ['the','a','an','but','of','and','for','at','by','from']
+    let answer = []
+    let myArray = string.split(" ")
+
+    for(let i = 0; i < myArray.length;i++){
+      if(i == 0){
+        answer.push(this.capitalize(myArray[i]))
+      }else{
+        if(exceptions.includes(myArray[i])){
+          answer.push(myArray[i])
+        }else{
+          answer.push(this.capitalize(myArray[i]))
+        }
+      }
+    }
+    return answer.join(' ')
+  }
+
 }
